ae71233172b113b46b25eef74d2532e16d0f8771
Jesus Maria Vargas Logreira <134142244+JesusVargasLogreira@users.noreply.github.com>
2026-05-08 19:02:37 +0000

    corregir errores typescript

--- app/dos/hijo.tsx
+++ app/dos/hijo.tsx
@@ -1,4 +1,6 @@
-export default function Hijo({ children }) {
+import { ReactNode } from 'react';
+
+export default function Hijo({ children }: { children: ReactNode }) {
     return (
         <div>
             este es mi hijo {children}
